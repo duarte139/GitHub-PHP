512d12fe147dd51dab7faa8f4d27216832b4381d
duarte139 <34600086+duarte139@users.noreply.github.com>
2018-01-06 10:58:12 -0200

    Adicionei um foreach

---
+++ exemplo-1.php
@@ -0,0 +1,33 @@
+	<?php
+
+		$meses =  array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
+		"Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+		 );
+
+		foreach ($meses as $mes) {
+
+			    
+			echo "O mês é: " .$mes."<br><br>";
+
+			if($mes=="Março"){
+
+				echo "E meu aniversario porra....." . "<br><br>";
+
+
+			}
+		}
+
+	?>
+
+
+
+
+
+
+
+
+
+
+
+
+
